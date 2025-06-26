c7dc7c44667f662c6a51901bd562af62442f14f2
Nishitha571 <nishithaingilala30@gmail.com>
2025-06-26 13:05:21 +0530

    CUSTOMER DETAILS

---
+++ Application_Form/CRM_Project/CustomerDetails.java
@@ -0,0 +1,35 @@
+package CRM_Project;
+
+public class CustomerDetails {
+    public static void main(String[] args) {
+        byte customerid = 101;
+        String firstname = "Nishitha";
+        String lastname = " Ingilala";
+        String email = "nishithaingilala30@gmail.com";
+        long phonenumber = 6301073808L;
+        String birthofyear = "20-08-2004";
+        String address = "Kavali, Andhra Pradesh, India";
+        char gender = 'F';
+        byte age = 20;
+        String country = "India";
+        int pincode = 524201;
+        boolean isActive = true;
+        String created_date = "11-08-2024";
+        
+        // print customer details
+        System.out.println("Customerid is " + customerid);
+        System.out.println("Firstname is " + firstname);
+        System.out.println("Lastname is " + lastname);
+        System.out.println("Email is " + email);
+        System.out.println("Phonenumber is " + phonenumber);
+        System.out.println("Birthyear is " + birthofyear);
+        System.out.println("Address  is " + address);
+        System.out.println("Gender is " + gender);
+        System.out.println("Age is " + age);
+        System.out.println("Country is " + country);
+        System.out.println("Pincode is " + pincode);
+        System.out.println("Is Active " + isActive);
+        System.out.println("created_date is " + created_date);
+    }
+    
+}
